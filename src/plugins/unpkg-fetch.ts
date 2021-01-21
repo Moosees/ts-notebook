e89749224ee5ab36005ab1740fc5c39d7add8a1c
Moosees <linus.almgren@gmail.com>
2021-01-21 22:56:10 +0100

    add css import handling

--- src/plugins/unpkg-fetch.ts
+++ src/plugins/unpkg-fetch.ts
@@ -23,9 +23,26 @@ export const unpkgFetchPlugin = (input: string) => {
 
           const { data, request } = await axios.get(args.path);
 
+          const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+          const parsedCss =
+            fileType === 'css'
+              ? data
+                  .replace(/\n/g, '')
+                  .replace(/"/g, '\\"')
+                  .replace(/'/g, "\\'")
+              : '';
+          const contents =
+            fileType === 'css'
+              ? `
+            const style = document.createElement('style');
+            style.innerText = '${parsedCss}';
+            document.head.appendChild(style);
+          `
+              : data;
+
           const newPackage: esbuild.OnLoadResult = {
             loader: 'jsx',
-            contents: data,
+            contents,
             resolveDir: new URL('./', request.responseURL).pathname,
           };
 
