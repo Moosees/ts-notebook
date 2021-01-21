5bc77074c4217b911f2d76345514f37a01f3929a
Moosees <linus.almgren@gmail.com>
2021-01-21 22:22:16 +0100

    use esbuild filters to handle onResolve calls

--- src/plugins/unpkg-path.ts
+++ src/plugins/unpkg-path.ts
@@ -8,22 +8,21 @@ export const unpkgPathPlugin = (input: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js')
-          return { path: args.path, namespace: 'a' };
-
-        if (args.path.includes('./') && args.resolveDir)
-          return {
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href,
-            namespace: 'a',
-          };
-
-        return {
-          path: `https://unpkg.com/${args.path}`,
-          namespace: 'a',
-        };
-      });
+      // resolve input entry point
+      build.onResolve({ filter: /(^index\.js$)/ }, () => ({
+        path: 'index.js',
+        namespace: 'a',
+      }));
+      // resolve relative imports
+      build.onResolve({ filter: /^\.+\// }, (args: any) => ({
+        path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+        namespace: 'a',
+      }));
+      // resolve main package import
+      build.onResolve({ filter: /.*/ }, (args: any) => ({
+        path: `https://unpkg.com/${args.path}`,
+        namespace: 'a',
+      }));
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         if (args.path === 'index.js')
