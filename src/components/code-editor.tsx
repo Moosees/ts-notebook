8d027b03034936439a6120f7c427e6284bbc6bb9
Moosees <linus.almgren@gmail.com>
2021-01-26 01:29:07 +0100

    add code formatting with prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
 import Editor, { OnMount } from '@monaco-editor/react';
 import { editor } from 'monaco-editor';
+import prettier from 'prettier';
+import babel from 'prettier/parser-babel';
 import { useRef } from 'react';
 
 interface CodeEditorProps {
@@ -27,16 +29,35 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ defaultValue, onChange }) => {
     editor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
+  const handleFormat = () => {
+    const code = editorRef.current?.getValue();
+    const formatted =
+      code &&
+      prettier.format(code, {
+        parser: 'babel',
+        plugins: [babel],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+      });
+
+    formatted && editorRef.current.setValue(formatted);
+  };
+
   return (
-    <Editor
-      theme="vs-dark"
-      height="40vh"
-      width="50vw"
-      language="javascript"
-      defaultValue={defaultValue}
-      options={options}
-      onMount={handleEditorDidMount}
-    />
+    <div>
+      <button onClick={handleFormat}>Format code</button>
+      <Editor
+        theme="vs-dark"
+        height="40vh"
+        width="50vw"
+        language="javascript"
+        defaultValue={defaultValue}
+        options={options}
+        onMount={handleEditorDidMount}
+      />
+    </div>
   );
 };
 
