5865e6f5a00195e401301862de3d4f2ac0a73b2a
Moosees <linus.almgren@gmail.com>
2021-01-28 22:14:22 +0100

    refactor esbuild bundler into helper file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import 'bulmaswatch/slate/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
-import { unpkgFetchPlugin } from './plugins/unpkg-fetch';
-import { unpkgPathPlugin } from './plugins/unpkg-path';
+import { bundleCode } from './esbuild-helpers/code-bundler';
 
 const dummyCode = `import React from 'react';
   import ReactDOM from 'react-dom';
@@ -15,39 +13,12 @@ const dummyCode = `import React from 'react';
 `;
 
 const App = () => {
-  const esbuildRef = useRef<any>();
   const [editorCode, setEditorCode] = useState(dummyCode);
   const [previewCode, setPreviewCode] = useState('');
 
-  const startService = async () => {
-    esbuildRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.33/esbuild.wasm',
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const handleSubmit = async () => {
-    if (!esbuildRef.current) return;
-    try {
-      const result = await esbuildRef.current.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [unpkgPathPlugin(), unpkgFetchPlugin(editorCode)],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window',
-        },
-      });
-
-      setPreviewCode(result.outputFiles[0].text);
-    } catch (error) {
-      console.error(error);
-    }
+    const bundledCode = await bundleCode(editorCode);
+    setPreviewCode(bundledCode);
   };
 
   return (
---
+++ src/esbuild-helpers/code-bundler.ts
@@ -0,0 +1,27 @@
+import * as esbuild from 'esbuild-wasm';
+import { unpkgFetchPlugin } from './plugins/unpkg-fetch';
+import { unpkgPathPlugin } from './plugins/unpkg-path';
+
+let service: esbuild.Service;
+
+export const bundleCode = async (editorCode: string) => {
+  if (!service) {
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.33/esbuild.wasm',
+    });
+  }
+
+  const result = await service.build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    write: false,
+    plugins: [unpkgPathPlugin(), unpkgFetchPlugin(editorCode)],
+    define: {
+      'process.env.NODE_ENV': '"production"',
+      global: 'window',
+    },
+  });
+
+  return result.outputFiles[0].text;
+};
