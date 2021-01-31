2a074cb6af2c90a67f56829c6ff807426b432cc7
Moosees <linus.almgren@gmail.com>
2021-01-31 04:18:55 +0100

    add markdown editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import 'bulmaswatch/slate/bulmaswatch.min.css';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <div>
+      <TextEditor />
       <CodeCell />
     </div>
   );
---
+++ src/components/text-editor.tsx
@@ -0,0 +1,32 @@
+import Editor from '@uiw/react-md-editor';
+import { useEffect, useRef, useState } from 'react';
+
+const TextEditor: React.FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const clickOutside = (e: MouseEvent) => {
+      if (ref.current && e.target && ref.current.contains(e.target as Node))
+        return;
+
+      setIsEditing(false);
+    };
+
+    document.addEventListener('click', clickOutside, { capture: true });
+    return () =>
+      document.removeEventListener('click', clickOutside, { capture: true });
+  }, []);
+
+  return (
+    <div onClick={() => setIsEditing(true)} ref={ref}>
+      {isEditing ? (
+        <Editor value="" />
+      ) : (
+        <Editor.Markdown source={'# Header'} />
+      )}
+    </div>
+  );
+};
+
+export default TextEditor;
