c2f1343a3399f870b5598c7557e8bc0cfed0c105
Moosees <linus.almgren@gmail.com>
2021-02-03 23:57:53 +0100

    setup store and exports

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import cellsReducer from './cells.reducer';
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export type RootState = ReturnType<typeof reducers>;
+
+export default reducers;
