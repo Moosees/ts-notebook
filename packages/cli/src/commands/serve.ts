3f35cee8e437eef1c735fe117d596b9c9a40446f
Moosees <linus.almgren@gmail.com>
2021-02-23 05:48:40 +0100

    add handling of directory and filename extention with path

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
 import { serve } from 'local-api';
+import path from 'path';
 
 export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a local notebook')
   .option('-p, --port <number>', 'port to run server on', '4242')
-  .action((filename = 'notebook.js', options: { port: string }) =>
-    serve(parseInt(options.port), filename, '/')
-  );
+  .action((filename = 'notebook.js', options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename, '.js') + '.js', dir);
+  });
