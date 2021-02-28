9b09b11e2c3d305b38aaf57539f2a0c078d07a80
Moosees <linus.almgren@gmail.com>
2021-02-28 15:41:13 +0100

    add local-api cell handling routes

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -7,6 +7,7 @@ exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
 var http_proxy_middleware_1 = require("http-proxy-middleware");
 var path_1 = __importDefault(require("path"));
+var cells_1 = require("./routes/cells");
 var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
     var clientPath = require.resolve('local-client/build/index.html');
@@ -20,6 +21,7 @@ var serve = function (port, filename, dir, useProxy) {
     else {
         app.use(express_1.default.static(path_1.default.dirname(clientPath)));
     }
+    app.use(cells_1.createCellsRouter(filename, dir));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
